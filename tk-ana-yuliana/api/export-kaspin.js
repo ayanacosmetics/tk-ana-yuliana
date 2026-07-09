3dd0c989aa5649630dd243c9b2acf63c8390f12a
ayanacosmetics <budiwirayu0412@gmail.com>
2026-07-09 18:48:46 +0800

    fix api template path

--- tk-ana-yuliana/api/export-kaspin.js
+++ tk-ana-yuliana/api/export-kaspin.js
@@ -3,33 +3,22 @@ const path = require("path");
 
 module.exports = async (req, res) => {
   try {
-
     const filePath = path.join(
-     process.cwd(),
-     "templates",
-     "TEMPLATE_BARANG.xls"
+      __dirname,
+      "templates",
+      "TEMPLATE_BARANG.xls"
     );
 
     const buffer = fs.readFileSync(filePath);
 
-    res.setHeader(
-      "Content-Type",
-      "application/vnd.ms-excel"
-    );
-
-    res.setHeader(
-      "Content-Disposition",
-      "attachment; filename=TEMPLATE_BARANG.xls"
-    );
+    res.setHeader("Content-Type", "application/vnd.ms-excel");
+    res.setHeader("Content-Disposition", "attachment; filename=TEMPLATE_BARANG.xls");
 
     return res.status(200).send(buffer);
-
   } catch (err) {
-
     return res.status(500).json({
-      success:false,
-      message:err.message
+      success: false,
+      message: err.message
     });
-
   }
 };
